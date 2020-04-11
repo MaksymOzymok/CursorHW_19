95c9885219002a996d4cfce503d83888a12e7546
MaxOzymok <ozymokmax1@ukr.net>
2020-04-11 23:08:14 +0300

    fixed router

--- signform/src/index.js
+++ signform/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import SignIn from "./sign-in-form/sign-in";
 import SignUp from "./sign-up-form/sign-up";
 import ReactDOM from "react-dom"
-import {BrowserRouter,Switch,Route} from "react-router-dom"
+import {HashRouter,Switch,Route} from "react-router-dom"
 
 ReactDOM.render(
     <>
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route exact path="/" component={SignIn}/>
                 <Route path="/register" component={SignUp}/>
             </Switch>
 
-        </BrowserRouter>
+        </HashRouter>
     </>,document.getElementById('root'));
 
